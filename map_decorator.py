a2d2d6a514e129c2098aeaa63951904f291a48d8
Sonych <sonychnikolai@gmail.com>
2014-04-14 00:35:09 +0300

    implemented map_rq, map_yield, and time_me decorator

---
+++ map_decorator.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+import time
+
+
+# task 1
+# custom map
+
+def map_rq(func, sequence):
+    if sequence:
+        return [func(sequence[0])] + map_rq(func, sequence[1:])
+    else:
+        return []
+
+
+def map_yield(func, sequence):
+    for item in sequence:
+        yield func(item)
+
+
+# TODO: don't work, fix it
+# def map_rq_yield(func, sequence):
+#     result = []
+#     if sequence:
+#         result.append(func(sequence[0]))
+#         result.extend(map_rq_yield(func, sequence[1:]))
+#         yield result
+#     else:
+#         yield []
+
+
+# Task 2
+# Profiling decorator
+
+def time_me(time_func, statistic):
+    statistic['num_calls'] = 0
+    statistic['cum_time'] = 0
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            t1 = time_func()
+            func(*args, **kwargs)
+            t2 = time_func()
+            statistic['cum_time'] += t2 - t1
+            statistic['num_calls'] += 1
+        return wrapper
+    return decorator
