1cfe261137ff009e09adddd9ce2e7af449ffa1cd
Sonych <sonychnikolai@gmail.com>
2014-04-28 09:46:15 +0300

    RPC home work

---
+++ rpc.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+
+import socket
+import cPickle
+
+
+class RPCServer(object):
+
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('localhost', 5448))
+        self.socket.listen(1)
+        self.procedures = {}
+
+    def register(self, func):
+        self.procedures[func.func_name] = func
+
+    def unregister(self, name):
+        if name in self.procedures:
+            del self.procedures[name]
+
+    def serve_forever(self):
+        while True:
+            conn, addr = self.socket.accept()
+            data = cPickle.loads(conn.recv(4096))
+
+            if data['func'] in self.procedures:
+                func_name = data['func']
+                args = data['args']
+                kwargs = data['kwargs']
+
+                try:
+                    result = self.procedures[func_name](*args, **kwargs)
+                    conn.sendall(cPickle.dumps(result))
+                except TypeError:
+                    conn.sendall(cPickle.dumps('Incorrect arguments'))
+
+            else:
+                conn.sendall(cPickle.dumps('Procedure do not found'))
+
+        self.socket.close()
+
+
+class RPCClient(object):
+
+    def __getattr__(self, name):
+        def wrapper(*args, **kwargs):
+            data = {
+                'func': name,
+                'args': args,
+                'kwargs': kwargs,
+            }
+            data = cPickle.dumps(data)
+
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect(('localhost', 5448))
+            self.socket.sendall(data)
+            result = cPickle.loads(self.socket.recv(4096))
+            self.socket.close()
+
+            return result
+
+        return wrapper
